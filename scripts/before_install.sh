115f3a83085f3c95e913a505e785eb1109232864
Marcel <marcelmiro1@hotmail.com>
2021-11-10 21:24:06 +0000

    Fix before_install script command not found

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -10,9 +10,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 # Install global npm required packages
-npm i -g typescript
-npm i -g pm2
-pm2 udpate
+npm i -g typescript pm2
+sudo pm2 udpate
 
 # Create working directory if not exists
 DIR="/home/ec2-user/astrano-api"
