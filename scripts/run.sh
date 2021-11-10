2ffa80ef1d5cd2c5f7dc8795a1ded002285f74fe
Marcel <marcelmiro1@hotmail.com>
2021-11-10 21:56:54 +0000

    Fix scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Add node to startup 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Set NODE_ENV if exists
 if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Add node to startup 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Move to working directory
 cd /home/ec2-user/astrano-server
 
