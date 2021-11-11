e1933ab58c571a2d806815aac52e05731e6b924a
Marcel <marcelmiro1@hotmail.com>
2021-11-11 21:07:29 +0000

    Fix cookie domain

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,15 @@ app.use(cookieParser())
 
 app.use((req, res, next) => {
 	logger.info(`${req.method} ${req.originalUrl}`)
+
 	res.header('Content-Type', 'application/json;charset=UTF-8')
-	res.header('Access-Control-Allow-Credentials', 'true')
+    // @ts-ignore
+	res.header('Access-Control-Allow-Credentials', true)
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
 	)
+
 	next()
 })
 
