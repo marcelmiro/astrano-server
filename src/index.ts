18c5e2f26878d178ed85308f549d03c7d5b63aeb
Marcel <marcelmiro1@hotmail.com>
2021-11-11 21:16:42 +0000

    Fix cookie domain

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express()
 const PORT: number = +(process.env.PORT || 8080)
 
 app.use(helmet())
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
