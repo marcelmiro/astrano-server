e487e783440686b8aa932f7a3f34fa0a44892fb6
Marcel <marcelmiro1@hotmail.com>
2021-11-12 09:25:36 +0000

    Add cookie domain env variable

--- src/config/csrf.config.ts
+++ src/config/csrf.config.ts
@@ -4,47 +4,51 @@ import { Request } from 'express'
 import { originUrl } from './index.config'
 
 if (!process.env.CSRF_TOKEN_COOKIE)
-    throw new Error('CSRF_TOKEN_COOKIE environmental variable not found')
+	throw new Error('CSRF_TOKEN_COOKIE environmental variable not found')
 
 export const csrfTokenCookie = process.env.CSRF_TOKEN_COOKIE
 
+const cookieDomain =
+	process.env.COOKIE_DOMAIN ||
+	originUrl.replace(/^https?:\/\//, '').replace(/:\d+/, '') // Remove protocol and port
+
 export const cookieDefaults: CookieOptions = {
-    sameSite: true,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    domain: originUrl.replace(/^https?:\/\//, '').replace(/:\d+/, '')
+	sameSite: true,
+	httpOnly: true,
+	secure: process.env.NODE_ENV === 'production',
+	domain: cookieDomain,
 }
 
 const ignoreMethods = ['HEAD', 'OPTIONS']
 
 const ignoreAllMethods = [
-    'CHECKOUT',
-    'COPY',
-    'DELETE',
-    'GET',
-    'HEAD',
-    'LOCK',
-    'MERGE',
-    'MKACTIVITY',
-    'MKCOL',
-    'MOVE',
-    'M-SEARCH',
-    'NOTIFY',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PURGE',
-    'PUT',
-    'REPORT',
-    'SEARCH',
-    'SUBSCRIBE',
-    'TRACE',
-    'UNLOCK',
-    'UNSUBSCRIBE',
+	'CHECKOUT',
+	'COPY',
+	'DELETE',
+	'GET',
+	'HEAD',
+	'LOCK',
+	'MERGE',
+	'MKACTIVITY',
+	'MKCOL',
+	'MOVE',
+	'M-SEARCH',
+	'NOTIFY',
+	'OPTIONS',
+	'PATCH',
+	'POST',
+	'PURGE',
+	'PUT',
+	'REPORT',
+	'SEARCH',
+	'SUBSCRIBE',
+	'TRACE',
+	'UNLOCK',
+	'UNSUBSCRIBE',
 ]
 
 export const csrfOptions = (ignore = false) => ({
-    cookie: cookieDefaults,
-    ignoreMethods: ignore ? ignoreAllMethods : ignoreMethods,
-    value: (req: Request) => req.cookies[csrfTokenCookie],
+	cookie: cookieDefaults,
+	ignoreMethods: ignore ? ignoreAllMethods : ignoreMethods,
+	value: (req: Request) => req.cookies[csrfTokenCookie],
 })
